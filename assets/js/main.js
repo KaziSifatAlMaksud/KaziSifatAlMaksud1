b8ced9b719a40194831a18f9bae801690451505e
KaziSifatAlMaksud <kazi.sifat2013@gmail.com>
2022-10-07 23:08:05 +0600

    add edu

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,6 +59,31 @@
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
+  /**
+   * eduction to inter change the  element with click
+   */
+
+   const contents = document.querySelectorAll('.content')
+   const listItems = document.querySelectorAll('.edu nav ul li')
+   
+   listItems.forEach((item, idx) => {
+       item.addEventListener('click', () => {
+           hideAllContents()
+           hideAllItems()
+   
+           item.classList.add('active')
+           contents[idx].classList.add('show')
+       })
+   })
+   
+   function hideAllContents() {
+       contents.forEach(content => content.classList.remove('show'))
+   }
+   
+   
+   function hideAllItems() {
+       listItems.forEach(item => item.classList.remove('active'))
+   }
 
   /**
    * Scrolls to an element with header offset
