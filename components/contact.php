4b2201e48dbb5c9ddc0c147175d88fcd0fbcfe78
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2023-06-03 00:48:29 +0600

    image uptimigation

--- components/contact.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-// local host
-$db_name = 'mysql:host=localhost;dbname=Lgorithm_lgorithmbd';
-$user_name = 'Lgorithm_app';
-$user_password = '{P#=uOCowm9i';
-
-// connection variable
-$conn = new PDO($db_name, $user_name, $user_password);
